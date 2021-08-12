cc77a3c2aaba3d87ff86d783ed6173b05bccc728
mazhuo <wb-mz865927@alibaba-inc.com>
2021-08-12 11:50:35 +0800

    react中setState原理入门

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 ### terminalCommand
 [/src/terminalCommand] 文件里存放的是 终端命令 后续会添加更多常用命令
+
+### styleReactPrinciple
+[/src/styleReactPrinciple] 文件里存放的是 react 中底层原理入门(具体内容如下)
+## setStateStyle
+[/components/setStateStyle] 文件里存放的是 setState 异步
--- src/styleReactPrinciple/styleReactPrinciple.js
+++ src/styleReactPrinciple/styleReactPrinciple.js
@@ -5,6 +5,7 @@ export default class styleReactPrinciple extends React.Component {
     render() {
         return (
         <div><div>3. react 原理入门</div>
+            {/* 查看时可注释掉所有，一个一个查看 */}
             <SetStateStyle /> {/* setState 的方法说明 */}
         </div>
         )
